306e441b929a97a873910ec7a462f158c6aae3fb
kenan238 <73741117+kenan238@users.noreply.github.com>
2023-01-23 19:54:00 +0200

    i love editing in github

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,3 +1,33 @@
 #include "Vector2.hpp"
+#include <cmath>
 
-namespace neoskye {} // namespace neoskye
+namespace neoskye {
+
+Vector2 Vector2::operator+(Vector2 const& other) {
+  return Vector2(this->x + other.x, this->y + other.y);
+}
+
+Vector2 Vector2::operator-(Vector2 const& other) {
+  return Vector2(this->x - other.x, this->y - other.y);
+}
+  
+Vector2 Vector2::operator*(Vector2 const& other) {
+  return Vector2(this->x * other.x, this->y * other.y);
+}
+  
+Vector2 Vector2::operator/(Vector2 const& other) { // if we're dividing by zero blame the user 😎
+  return Vector2(this->x / other.x, this->y / other.y);
+}
+  
+double Vector2::mag() {
+  return sqrt(this->x*this->x + this->y*this->y);
+}
+  
+void Vector2::normalize() {
+  double magnitude = this->mag();
+  
+  this->x = this->x / magnitude;
+  this->y = this->y / magnitude;
+}
+  
+} // namespace neoskye
