00c8f8f47cb8b5bb694513b9f6ef2f6f43100201
zTags <wierdgeomery@gmail.com>
2023-01-29 12:49:36 +0100

    number types

--- src/neoskye/util/Types.hpp
+++ src/neoskye/util/Types.hpp
@@ -1,18 +1,20 @@
 #pragma once
 
-#define u8 std::uint8_t
-#define u16 std::uint16_t
-#define u32 std::uint32_t
-#define u64 std::uint64_t
-#define u128 std::uint128_t
-#define usize std::uintptr_t
+#include <cstdint>
 
-#define i8 std::int8_t
-#define i16 std::int16_t
-#define i32 std::int32_t
-#define i64 std::int64_t
-#define i128 std::int128_t
-#define isize std::intptr_t
+#define u8 uint8_t
+#define u16 uint16_t
+#define u32 uint32_t
+#define u64 uint64_t
+#define u128 uint128_t
+#define usize uintptr_t
+
+#define i8 int8_t
+#define i16 int16_t
+#define i32 int32_t
+#define i64 int64_t
+#define i128 int128_t
+#define isize intptr_t
 
 #define f32 float
 #define f64 double
