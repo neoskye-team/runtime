2b2716419303f401501c069afbda1114e30334c2
kenan238 <73741117+kenan238@users.noreply.github.com>
2023-02-04 21:27:03 +0200

    quite the vector2

--- src/neoskye/Vector2.cpp
+++ src/neoskye/Vector2.cpp
@@ -1,4 +1,5 @@
 #include "neoskye/Vector2.hpp"
+#include "../util/Functions.hpp"
 #include <cmath>
 
 namespace neoskye {
@@ -37,5 +38,69 @@ void Vector2<T>::Normalize() {
     this->x = this->x / magnitude;
     this->y = this->y / magnitude;
 }
+    
+template <typename T>
+Vector2<T> Vector2<T>::Abs() {
+    return Vector2(abs(this->x), abs(this->y));
+}
+    
+template <typename T>
+float Vector2<T>::Angle() { // in radians
+    return atan2(this->y, this->x);
+}
+    
+template <typename T>    
+float Vector2<T>::Cross(const Vector2 b) {
+    return this->x * b.y - y * b.x;
+}
+   
+    
+template <typename T>
+float Vector2<T>::Dot(const Vector2 with) {
+    return this->x * with.x + this->y * with.y;
+}
+    
+template <typename T>
+float Vector2<T>::AngleTo(const Vector2 to) { // in radians
+    return atan2(this->Cross(to), this->Dot(to));
+}
+    
+template <typename T>
+float Vector2<T>::AngleToPoint(const Vector2 to) {
+    return atan2(this->y - to.y, this->x - to.x);
+}
+    
+template <typename T>
+float Vector2<T>::Aspect() {
+    return this->x / this->y;
+}
+    
+template <typename T>
+Vector2<T> Vector2<T>::Inverse() {
+    return Vector2(1 / this->x, 1 / this->y);
+}
+    
+template <typename T>
+Vector2<T> Vector2<T>::Perpendicular() {
+    return Vector2(this->y, this->x);
+}
 
+template <typename T>
+float Vector2<T>::Length() {
+    return sqrt(this->x * this->x + this->y * this->y);
+}
+    
+template <typename T>
+float Vector2<T>::LengthSquared() {
+    float l = this->Length();
+    return l * l;
+}
+
+template <typename T>
+Vector2<T> Vector2<T>::LinearInterpolate(const Vector2 to, float weight) {
+    float lerpedX = util::lerp(this->x, to.x, weight);
+    float lerpedY = util::lerp(this->y, to.y, weight);
+    return Vector2(lerpedX, lerpedY);
+}
+    
 } // namespace neoskye
